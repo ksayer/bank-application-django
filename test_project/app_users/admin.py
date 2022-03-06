38d621da28cc997d4d2438f394e8922e307985cd
Nikolai Zinchenko <ksayer@yandex.ru>
2022-03-06 14:09:13 +0300

    added models,added models in admin-panel

--- test_project/app_users/admin.py
+++ test_project/app_users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Wallet, Transaction, Transfer
 
-# Register your models here.
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'balance', 'name']
+    list_filter = ['user']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'receiver', 'sender']
+    list_filter = ['date', 'receiver', 'sender']
+
+
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ['id', 'wallet', 'operation', 'date']
+    list_filter = ['wallet', 'date']
+
+
+admin.site.register(Wallet, WalletAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Transfer, TransferAdmin)
+
+
+
+
+
--- test_project/app_users/models.py
+++ test_project/app_users/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Wallet(models.Model):
+    """Модель счёта пользователя"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wallets', verbose_name='Пользователь')
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0, verbose_name='Баланс')
+    name = models.CharField(max_length=128, verbose_name='Название')
+
+    def __str__(self):
+        return f"{self.name}({self.user})"
+
+    class Meta:
+        verbose_name = 'счёт'
+        verbose_name_plural = 'счета'
+
+
+class Transaction(models.Model):
+    """Модель транзакции"""
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions_receiver', verbose_name='Получатель')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions_sender', verbose_name='Отправитель')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата')
+
+    def __str__(self):
+        return f"ID{self.id}.{self.date}: {self.sender} -> {self.receiver}"
+
+    class Meta:
+        verbose_name = 'транзакция'
+        verbose_name_plural = 'транзакции'
+
+
+class Transfer(models.Model):
+    """Модель произведения операции с кошельком (изменения баланса)"""
+    OPERATION_CHOICES = [
+        ('r', 'Пополнение'),
+        ('s', 'Снятие')
+    ]
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE,
+                                    related_name='transfers', verbose_name='Транзакция')
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='transfers', verbose_name='Счёт')
+    number = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Количество')
+    operation = models.CharField(max_length=1, choices=OPERATION_CHOICES, verbose_name='Операция')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата')
+
+    def __str__(self):
+        return f"{self.id}.{self.wallet}"
+
+    class Meta:
+        verbose_name = 'перевод'
+        verbose_name_plural = 'переводы'
+
+
